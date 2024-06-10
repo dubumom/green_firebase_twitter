a59e2e6e32eb9316a9500eea863afe7ce419b6ad
dubumom <alslwjdk@gmail.com>
2024-06-10 13:17:42 +0900

    로그인여부파

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
 import '../App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from './Router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <AppRouter isLoggedIn = {isLoggedIn}/>
+  const [init, setInit] = useState(false);
+  const auth = getAuth();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true)
+        //const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        setIsLoggedIn(false)
+      }
+      setInit(true);
+    });
+  },[])
+
+  return(
+    <>
+    {init ? 
+      <AppRouter isLoggedIn = {isLoggedIn}/>
+      :"초기화 중..."
+    }
+    </>
+  )
+  
 }
 export default App;
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [passWord, setPassWord] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
   const auth = getAuth();
 
   //input에 값이 바뀔 때 마다 할일
@@ -37,23 +38,45 @@ const Auth = () => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(errorCode,errorMessage);
+          setError(errorMessage);
         });
     } else {
       //로그인
-
+      signInWithEmailAndPassword(auth, email, passWord)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode,errorMessage);
+        setError(errorMessage);
+      });
     }
   }
   
   console.log(email,passWord);
 
+  const toggleAccount =()=>{
+    //setNewAccount(prev=>!prev)
+    setNewAccount(!newAccount);
+  }
   return(
-    <form onSubmit={onsubmit}>
-      <input type="email" name="email" placeholder="email" onChange={onchange}/>
-      <input type="password" name="password" placeholder="password" onChange={onchange}/>
-      <p>
-        <button>{newAccount ? '회원가입' : '로그인'}</button>
-      </p>
-    </form>
+    <>
+      <form onSubmit={onsubmit}>
+        <input type="email" name="email" placeholder="email" onChange={onchange}/>
+        <input type="password" name="password" placeholder="password" onChange={onchange}/>
+        <p>
+          <button>{newAccount ? '회원가입' : '로그인'}</button>
+        </p>
+        {error}
+      </form>
+      <hr/>
+      <button type="button" onClick={toggleAccount}>{newAccount ? '로그인 바로가기' : '회원가입 바로가기'}</button>
+    </>
+
   )
 }
 
