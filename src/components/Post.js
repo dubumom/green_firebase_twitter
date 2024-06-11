7751abcacb106d7d261e4e4ef09c2af8d10f954f
dubumom <alslwjdk@gmail.com>
2024-06-11 12:34:26 +0900

    데이터삭제

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { db } from '../firebase';
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Post = ({postObj, isOwener}) => {
-  const deletePost = ()=>{
+  const deletePost = async ()=>{
     const question = window.confirm('정말 삭제할까요?'); //한번 더 묻는 함수
     if(question){
-
+      await deleteDoc(doc(db, "posts", postObj.id));
     }
   }
   return(
