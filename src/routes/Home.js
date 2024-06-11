b3f6aee03c6430bc36e176d95a57c653d1095f19
dubumom <alslwjdk@gmail.com>
2024-06-11 11:01:43 +0900

    list 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from '../firebase';
-import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp, getDocs } from "firebase/firestore"; 
 
 const Home = () => {
     const [post,setPost] = useState('');
+    const [posts,setPosts] = useState([]);
     const onchange = (e) =>{
       //let value = e.target.value
       const {target:{value}} = e; //비구조 할당
@@ -23,12 +24,36 @@ const Home = () => {
         console.error("Error adding document: ", e);
       }
     }
+
+    const getPosts = async () =>{
+      const querySnapshot = await getDocs(collection(db,"posts"));
+      querySnapshot.forEach((doc) => {
+        //console.log(doc.data().post);
+        const postObj = {
+          ...doc.data(),
+          id : doc.id
+        }
+        setPosts(prev => [postObj, ...prev])
+      });
+    }
+    console.log(posts)
+    useEffect(()=>{
+      getPosts();
+    },[]);
+
+    //let list = posts.map(item=><li>{item.post}</li>);
+
     return(
       <div>
         <form action="" onSubmit={onsubmit}>
           <input type="text" name="" value={post} placeholder="내용을 입력하세요" onChange={onchange} />
           <input type="submit" value="Add Post" />
         </form>
+        <ul>
+          {
+            posts.map(item=><li key={item.id}>{item.post}</li>)
+          }
+        </ul>
       </div>
     )
   }
