74edd5be3a0f7a9cf4d08a8f7f452a35ecb53ecd
dubumom <alslwjdk@gmail.com>
2024-06-11 12:57:14 +0900

    수정

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { db } from '../firebase';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 const Post = ({postObj, isOwener}) => {
+  const [edit,setEdit] = useState(false);
+  const [newPost, setNewPost] = useState(postObj.content);
+
+  //데이터 삭제
   const deletePost = async ()=>{
     const question = window.confirm('정말 삭제할까요?'); //한번 더 묻는 함수
     if(question){
       await deleteDoc(doc(db, "posts", postObj.id));
     }
   }
+  //데이터 수정
+  const toggleEditMode = () => setEdit(prev => !prev);
+  const onchange = (e) =>{
+    const {target:{value}} = e;
+    setNewPost(value); // 내용 변경
+  }
+  const onsubmit = async(e) =>{ // 내용 수정 완료
+    e.preventDefault()
+    const postRef = doc(db, "posts", postObj.id);
+    await updateDoc(postRef, {
+      content: newPost
+    });
+    setEdit(false);
+  }
   return(
     <li>
-      <h4>{postObj.content}</h4>
       {
-      isOwener && (
+      edit ? (
         <>
-          <button>수정</button>
-          <button onClick={deletePost}>삭제</button>
+        <form onSubmit={onsubmit}>
+          <input value={newPost} onChange={onchange}/>
+          <button>수정완료</button>
+        </form>
+        <button onClick={toggleEditMode}>취소</button>
         </>
-      )
-      }
+      ) : (
+      <>
+        <h4>{postObj.content}</h4>
+        {
+        isOwener && (
+          <>
+            <button onClick={toggleEditMode}>수정</button>
+            <button onClick={deletePost}>삭제</button>
+          </>
+        )
+        }
+      </>
+    )}
     </li>
   )
 }
