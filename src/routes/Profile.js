afcf87ab8281a58135cf8b328575eaaf63689bff
dubumom <alslwjdk@gmail.com>
2024-06-12 12:21:35 +0900

    profile 생성

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const Profile = () => {
   const auth = getAuth();
@@ -15,6 +16,11 @@ const Profile = () => {
 
   return(
     <>
+    <div className="profile">
+      <img src={`${process.env.PUBLIC_URL}/profile_img.svg`} alt="" />
+      <input type="file" className="hidden" id="profile" accept="image/*" />
+      <label for="profile">프로필 수정</label>
+    </div>
       <button onClick={logout}>Log Out</button>
     </>
   )
